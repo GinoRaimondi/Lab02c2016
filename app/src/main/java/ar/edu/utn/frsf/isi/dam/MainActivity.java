efffbf808828b2f1269255c7d9aa48520d7df977
Denis Bevilacqua <denisbevilacqua6@gmail.com>
2016-09-12 19:07:19 -0300

    Se agregan los eventos para los botones agregar y reiniciar. Se actualiza el textview con los elementos agregados al pedido.

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/MainActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/MainActivity.java
@@ -19,6 +19,9 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,9 +36,18 @@ public class MainActivity extends AppCompatActivity {
     ElementoMenu[] listaPlatos;
     ElementoMenu[] listaPostre;
 
+    List<ElementoMenu> pedidoArmado = new ArrayList<ElementoMenu>();
+    ;
+    String item;
+
     ListView listView;
 
     ToggleButton toggle;
+
+    Button agregar;
+    Button botonReiniciar;
+
+    TextView listadoAPedir;
 
 
     private RadioGroup radioGroup;
@@ -44,8 +56,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = (ListView)findViewById(R.id.listViewElementoMenu);
-        toggle = (ToggleButton)findViewById(R.id.toggleButton);
+        listView = (ListView) findViewById(R.id.listViewElementoMenu);
+        toggle = (ToggleButton) findViewById(R.id.toggleButton);
+        agregar = (Button) findViewById(R.id.botonAgregar);
+        botonReiniciar = (Button) findViewById(R.id.botonReiniciar);
+        listadoAPedir = (TextView) findViewById(R.id.listadoAPedir);
+
         iniciarListas();
 
         final ArrayList array = new ArrayList();
@@ -53,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
         final ArrayAdapter adapter = new ArrayAdapter<ArrayList>(this, android.R.layout.simple_list_item_single_choice);
 
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-
 
         toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -66,38 +81,79 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        botonReiniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Debemos borrar el listado a pedir y actualizar
+
+                pedidoArmado.clear();
+                actualizarListaPedidos();
+
+                Toast.makeText(getBaseContext(), "Se ha reiniciado su pedido ", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, final int position,
+                                    long id) {
+
+                agregar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        /* ElementoMenu elementoMenu = (ElementoMenu) listView.getSelectedItem();
+                         Toast.makeText(getBaseContext(), elementoMenu.toString(), Toast.LENGTH_LONG).show();*/
+
+                        //Toast.makeText(getBaseContext(), item, Toast.LENGTH_LONG).show();
+                        // item Es el texto del elemento seleccionado.
+                        // Debemos agregarlo a una lista o al textview
+
+                        // Elemento pedido
+                        ElementoMenu elementoMenu = (ElementoMenu) listView.getItemAtPosition(position);
+
+                        item = ((TextView) view).getText().toString();
+
+                        // Mostramos todos los datos guardados
+                        Toast.makeText(getBaseContext(), "Ha agregado " + elementoMenu.getNombre(), Toast.LENGTH_SHORT).show();
+
+
+                        pedidoArmado.add(elementoMenu);
+
+                        actualizarListaPedidos();
+                        //Toast.makeText(getBaseContext(), item, Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+
+            }
+        });
+
+        listadoAPedir.setMovementMethod(new ScrollingMovementMethod());
+
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 // find which radio button is selected
-                if(checkedId == R.id.radioButtonPlato) {
+                if (checkedId == R.id.radioButtonPlato) {
 
+                    mostrarLista(array, adapter, listaPlatos, "Usted ha seleccionado platos");
 
-                    array.clear();
-                    array.addAll(Arrays.asList(listaPlatos));
-                    adapter.clear();
-                    adapter.addAll(array);
-                    adapter.notifyDataSetChanged();
-                    listView.setAdapter(adapter);
-                   // listView.setOnItemClickListener(new OnItemClickListenerListViewItem());
+                } else if (checkedId == R.id.radioButtonPostre) {
 
+                    mostrarLista(array, adapter, listaPostre, "Usted ha seleccionado postres");
 
-                    Toast.makeText(getApplicationContext(), "choice: plato",
-                            Toast.LENGTH_SHORT).show();
-                } else if(checkedId == R.id.radioButtonPostre) {
-                    array.clear();
-                    array.addAll(Arrays.asList(listaPostre));
-                    adapter.clear();
-                    adapter.addAll(array);
-                    adapter.notifyDataSetChanged();
-                    listView.setAdapter(adapter);
+                } else if (checkedId == R.id.radioButtonBebida) {
 
-                    Toast.makeText(getApplicationContext(), "choice: postre",
-                            Toast.LENGTH_SHORT).show();
-                }else if(checkedId == R.id.radioButtonBebida) {
-                    Toast.makeText(getApplicationContext(), "choice: bebida",
-                            Toast.LENGTH_SHORT).show();
+                    mostrarLista(array, adapter, listaBebidas, "Usted ha seleccionado bebidas");
+
                 } else {
                     Toast.makeText(getApplicationContext(), "choice: Vibration",
                             Toast.LENGTH_SHORT).show();
@@ -106,6 +162,29 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+    }
+
+    private void mostrarLista(ArrayList array, ArrayAdapter adapter, ElementoMenu[] lista, String texto) {
+
+
+        listView.clearChoices();
+        listView.setItemChecked(-1, true);
+        //listView.setAdapter(null);
+
+        // Limpiamos el array y le cargamos la lista con los elementos a mostrar
+        array.clear();
+        array.addAll(Arrays.asList(lista));
+
+        // Limpiamos el adapter
+        adapter.clear();
+        adapter.addAll(array);
+        adapter.notifyDataSetChanged();
+        listView.setAdapter(adapter);
+        // listView.setOnItemClickListener(new OnItemClickListenerListViewItem());
+
+        // Mostramos un mensaje indicando el tipo de comida mostrada en la lista
+        Toast.makeText(getApplicationContext(), texto,
+                Toast.LENGTH_SHORT).show();
     }
 
     class ElementoMenu {
@@ -123,9 +202,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public ElementoMenu(Integer i, String n) {
-            this(i,n,0.0);
+            this(i, n, 0.0);
             Random r = new Random();
-            this.precio= (r.nextInt(3)+1)*((r.nextDouble()*100));
+            this.precio = (r.nextInt(3) + 1) * ((r.nextDouble() * 100));
         }
 
 
@@ -155,53 +234,75 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public String toString() {
-            return this.nombre+ "( "+f.format(this.precio)+")";
+            return this.nombre + "( " + f.format(this.precio) + ")";
         }
     }
 
-    private void iniciarListas(){
+    private void actualizarListaPedidos() {
+        // Llega una lista, y la debemos mostrar en el textview
+        listadoAPedir.setText("");
+
+        StringBuilder builder = new StringBuilder();
+
+
+        for (int i = 0; i < pedidoArmado.size(); i++) {
+
+            Integer elemento = i+1;
+            builder.append("Elemento " + elemento + " : " + pedidoArmado.get(i).toString() + "\n");
+
+            // listadoAPedir.append("Elemento " + i  + " : " + pedidoArmado.get(i).toString() + "\n");
+
+        }
+
+        listadoAPedir.setText(builder.toString());
+
+
+    }
+
+    private void iniciarListas() {
+
         // inicia lista de bebidas
         listaBebidas = new ElementoMenu[7];
-        listaBebidas[0]=new ElementoMenu(1,"Coca");
-        listaBebidas[1]=new ElementoMenu(4,"Jugo");
-        listaBebidas[2]=new ElementoMenu(6,"Agua");
-        listaBebidas[3]=new ElementoMenu(8,"Soda");
-        listaBebidas[4]=new ElementoMenu(9,"Fernet");
-        listaBebidas[5]=new ElementoMenu(10,"Vino");
-        listaBebidas[6]=new ElementoMenu(11,"Cerveza");
+        listaBebidas[0] = new ElementoMenu(1, "Coca");
+        listaBebidas[1] = new ElementoMenu(4, "Jugo");
+        listaBebidas[2] = new ElementoMenu(6, "Agua");
+        listaBebidas[3] = new ElementoMenu(8, "Soda");
+        listaBebidas[4] = new ElementoMenu(9, "Fernet");
+        listaBebidas[5] = new ElementoMenu(10, "Vino");
+        listaBebidas[6] = new ElementoMenu(11, "Cerveza");
         // inicia lista de platos
-        listaPlatos= new ElementoMenu[14];
-        listaPlatos[0]=new ElementoMenu(1,"Ravioles");
-        listaPlatos[1]=new ElementoMenu(2,"Gnocchi");
-        listaPlatos[2]=new ElementoMenu(3,"Tallarines");
-        listaPlatos[3]=new ElementoMenu(4,"Lomo");
-        listaPlatos[4]=new ElementoMenu(5,"Entrecot");
-        listaPlatos[5]=new ElementoMenu(6,"Pollo");
-        listaPlatos[6]=new ElementoMenu(7,"Pechuga");
-        listaPlatos[7]=new ElementoMenu(8,"Pizza");
-        listaPlatos[8]=new ElementoMenu(9,"Empanadas");
-        listaPlatos[9]=new ElementoMenu(10,"Milanesas");
-        listaPlatos[10]=new ElementoMenu(11,"Picada 1");
-        listaPlatos[11]=new ElementoMenu(12,"Picada 2");
-        listaPlatos[12]=new ElementoMenu(13,"Hamburguesa");
-        listaPlatos[13]=new ElementoMenu(14,"Calamares");
+        listaPlatos = new ElementoMenu[14];
+        listaPlatos[0] = new ElementoMenu(1, "Ravioles");
+        listaPlatos[1] = new ElementoMenu(2, "Gnocchi");
+        listaPlatos[2] = new ElementoMenu(3, "Tallarines");
+        listaPlatos[3] = new ElementoMenu(4, "Lomo");
+        listaPlatos[4] = new ElementoMenu(5, "Entrecot");
+        listaPlatos[5] = new ElementoMenu(6, "Pollo");
+        listaPlatos[6] = new ElementoMenu(7, "Pechuga");
+        listaPlatos[7] = new ElementoMenu(8, "Pizza");
+        listaPlatos[8] = new ElementoMenu(9, "Empanadas");
+        listaPlatos[9] = new ElementoMenu(10, "Milanesas");
+        listaPlatos[10] = new ElementoMenu(11, "Picada 1");
+        listaPlatos[11] = new ElementoMenu(12, "Picada 2");
+        listaPlatos[12] = new ElementoMenu(13, "Hamburguesa");
+        listaPlatos[13] = new ElementoMenu(14, "Calamares");
         // inicia lista de postres
-        listaPostre= new ElementoMenu[15];
-        listaPostre[0]=new ElementoMenu(1,"Helado");
-        listaPostre[1]=new ElementoMenu(2,"Ensalada de Frutas");
-        listaPostre[2]=new ElementoMenu(3,"Macedonia");
-        listaPostre[3]=new ElementoMenu(4,"Brownie");
-        listaPostre[4]=new ElementoMenu(5,"Cheescake");
-        listaPostre[5]=new ElementoMenu(6,"Tiramisu");
-        listaPostre[6]=new ElementoMenu(7,"Mousse");
-        listaPostre[7]=new ElementoMenu(8,"Fondue");
-        listaPostre[8]=new ElementoMenu(9,"Profiterol");
-        listaPostre[9]=new ElementoMenu(10,"Selva Negra");
-        listaPostre[10]=new ElementoMenu(11,"Lemon Pie");
-        listaPostre[11]=new ElementoMenu(12,"KitKat");
-        listaPostre[12]=new ElementoMenu(13,"IceCreamSandwich");
-        listaPostre[13]=new ElementoMenu(14,"Frozen Yougurth");
-        listaPostre[14]=new ElementoMenu(15,"Queso y Batata");
+        listaPostre = new ElementoMenu[15];
+        listaPostre[0] = new ElementoMenu(1, "Helado");
+        listaPostre[1] = new ElementoMenu(2, "Ensalada de Frutas");
+        listaPostre[2] = new ElementoMenu(3, "Macedonia");
+        listaPostre[3] = new ElementoMenu(4, "Brownie");
+        listaPostre[4] = new ElementoMenu(5, "Cheescake");
+        listaPostre[5] = new ElementoMenu(6, "Tiramisu");
+        listaPostre[6] = new ElementoMenu(7, "Mousse");
+        listaPostre[7] = new ElementoMenu(8, "Fondue");
+        listaPostre[8] = new ElementoMenu(9, "Profiterol");
+        listaPostre[9] = new ElementoMenu(10, "Selva Negra");
+        listaPostre[10] = new ElementoMenu(11, "Lemon Pie");
+        listaPostre[11] = new ElementoMenu(12, "KitKat");
+        listaPostre[12] = new ElementoMenu(13, "IceCreamSandwich");
+        listaPostre[13] = new ElementoMenu(14, "Frozen Yougurth");
+        listaPostre[14] = new ElementoMenu(15, "Queso y Batata");
 
     }
 }
